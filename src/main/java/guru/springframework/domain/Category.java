2bccc44226653139bb407010c2d89b0b6a312ba4
Lukas Brutschy <lukas.brutschy@gmail.com>
2021-06-11 11:00:02 +0200

    Exclude properties in equality and hash code methods

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -1,12 +1,14 @@
 package guru.springframework.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Data
+@EqualsAndHashCode(exclude = {"recipes"})
 public class Category {
 
     @Id
